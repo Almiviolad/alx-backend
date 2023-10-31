8272517559fc8e4f87b348a041e2fc9c82f82a32
root <mykelaloh@gmail.com>
2023-10-31 16:47:38 -0700

    pycodestyled andimorot any from typing

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 """ Flask app module tha prints welcome message"""
 from flask import Flask, render_template
+from typing import Any
+
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def home() -> Any:
     """outputs welcome message"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
