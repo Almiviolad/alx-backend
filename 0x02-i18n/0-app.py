dc32b564b6093e083db0f9c329ff08f7b7a54f06
root <mykelaloh@gmail.com>
2023-10-31 17:24:43 -0700

    task 1 add babel to app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,10 +2,18 @@
 """ Flask app module tha prints welcome message"""
 from flask import Flask, render_template
 from typing import Any
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+babel = Babel(app)
+class Config(object):
+    """configure available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
 
+app.config.from_object(Config)
 
 @app.route('/')
 def home() -> Any:
