c38d281384e45e5f25b8658b321bfbbdb3985ccf
root <mykelaloh@gmail.com>
2023-10-31 19:37:36 -0700

    task3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Flask app module tha prints welcome message"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from typing import Any
 from flask_babel import Babel
 
@@ -21,13 +21,13 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
-    request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/')
 def home() -> Any:
     """outputs welcome message"""
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
